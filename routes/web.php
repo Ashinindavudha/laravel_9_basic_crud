e10cfbb7c3bbf617fc51b4e22fd1f1d2920e91b9
Ashinindavudha <ashinindavudha2018@gmail.com>
2022-03-03 18:57:05 +0630

    second updated code

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\TaskController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,3 +18,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::resource("student", StudentController::class);
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+//Route::resource('tasks', TaskController::class);
+Route::group(['middleware' => 'auth'], function () {
+    Route::resource('tasks', \App\Http\Controllers\TasksController::class);
+
+    Route::resource('users', \App\Http\Controllers\UserController::class);
+});
